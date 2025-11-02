35ecf9e58c7bb4ac860822102e11f88a8434ee57
noni44e <enzononi22@gmail.com>
2025-11-02 19:37:34 -0300

    actualización.

---
+++ js/space.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    const input = document.getElementById('inputBuscar');
+    const buscar = document.getElementById('btnBuscar');
+    const contenedor = document.getElementById('contenedor');
+    
+    buscar.addEventListener('click', ()=>{
+        const elemento = input.value.trim();
+        if (elemento === '')return;
+        document.getElementById("spinner").style.display = "block";
+        fetch(`https://images-api.nasa.gov/search?q=${encodeURIComponent(elemento)}&media_type=image`)
+        .then(response => response.json())
+        .then(data =>{
+            contenedor.innerHTML= ""; 
+
+            const item  = data.collection.items;
+            if(item.length===0){
+                contenedor.innerHTML="<p>No se encontaron resultados.</p>"
+                return;
+            }
+
+            item.forEach(ele=>{
+                const datos = ele.data[0];
+                const imagen = ele.links?.[0]?.href || "";
+                const titulo = datos.title || "Sin título";
+                const descripcion = datos.description || "Sin descripción";
+                const fecha = datos.date_created?.split("T")[0] || "Sin fecha";
+
+                const card = document.createElement("div");
+                card.className = "card bg-dark text-white m-2";
+                card.style.width = "18rem";
+
+                card.innerHTML = `
+                <img src="${imagen}" class="card-img-top" alt="${titulo}">
+                <div class="card-body">
+                <h5 class="card-title">${titulo}</h5>
+                <div class="card-text overflow-auto" style="max-height: 120px;">
+                ${descripcion}
+                </div>
+                <p class="card-text"><small class="text-muted">📅 ${fecha}</small></p>
+                </div>
+                `;
+
+                contenedor.appendChild(card);
+            });
+
+        });
+    });
+})
